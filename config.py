8644b4a95fcda9641511506b4c5608af285a69f2
Kevin <3627713050@qq.com>
2025-07-31 23:20:11 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
+import os
 Cookies = [
+    os.environ.get('cookie1'),
     #'koa:sess=XXXXXXXXXXXXXXXXXXXX; koa:sess.sig=XXXXXXXXXX;', #cookie1
     #'koa:sess=XXXXXXXXXXXXXXXXXXXX; koa:sess.sig=XXXXXXXXXX;', #cookie2
     ]
